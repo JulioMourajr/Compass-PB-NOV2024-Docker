6f0b771b66ac7929ebe3b1da4d5ecd5c95005e41
Julio Moura <juliomourajr@hotmail.com>
2025-02-03 18:49:40 -0300

    :recycle: refactor: add another ec2.

--- ec2.tf
+++ ec2.tf
@@ -20,13 +20,14 @@ resource "aws_instance" "wordpress-instance1a" {
   }
 }
 
-resource "aws_instance" "bastion" {
-  ami                    = "ami-0c614dee691cbbf37"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.publica-elb-A.id
-  vpc_security_group_ids = [aws_security_group.alb_sg.id]
-  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
-  key_name               = "wordpress-key"
+resource "aws_instance" "wordpress-instance1b" {
+  ami                         = "ami-0c614dee691cbbf37"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.privada-app-B.id
+  vpc_security_group_ids      = [aws_security_group.wordpress_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.name
+  associate_public_ip_address = true
+  key_name                    = "wordpress-key"
 
   tags = {
     Name       = "PB - NOV 2024"
