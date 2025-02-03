61d3a6dfc699f64de5807be4545c2a915cf54d0a
Julio Moura <juliomourajr@hotmail.com>
2025-02-03 11:34:00 -0300

    :sparkles: feat: create security group.

---
+++ security_group.tf
@@ -0,0 +1,103 @@
+resource "aws_security_group" "wordpress_sg" {
+  vpc_id      = aws_vpc.compass_vpc.id
+  description = "Security group for wordpress."
+  name        = "wordpress_sg"
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all traffic out"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wordpress_sg"
+  }
+
+}
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id      = aws_vpc.compass_vpc.id
+  description = "Security group for ALB."
+  name        = "alb_sg"
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all traffic out"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb_sg"
+  }
+
+}
+
+resource "aws_security_group" "rds_sg" {
+  vpc_id      = aws_vpc.compass_vpc.id
+  description = "Security group for rds."
+  name        = "rds_sg"
+
+  ingress {
+    description = "Allow service MySQL"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  tags = {
+    Name = "rds_sg"
+  }
+}
