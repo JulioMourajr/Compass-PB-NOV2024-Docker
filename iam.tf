68d35a64ba7037af56d9006db1274775dd6ab324
Julio Moura <juliomourajr@hotmail.com>
2025-02-03 14:46:24 -0300

    :sparkles: feat: create database.

---
+++ iam.tf
@@ -0,0 +1,78 @@
+resource "aws_iam_role" "ec2_role" {
+  name               = "ec2_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ec2_policy" {
+  name   = "ec2_policy"
+  role   = aws_iam_role.ec2_role.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_role" "rds_role" {
+  name               = "rds_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "rds.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "rds_policy" {
+  name   = "rds_policy"
+  role   = aws_iam_role.rds_role.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "rds:CreateDBInstance",
+        "rds:DescribeDBInstances",
+        "rds:DeleteDBInstance"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
---
+++ rds.tf
@@ -0,0 +1,30 @@
+resource "aws_db_instance" "rds" {
+  allocated_storage      = 20
+  db_name = "wordpress-db"
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  username               = "admin"
+  password               = "admin123"
+  parameter_group_name   = "default.mysql8.0"
+  publicly_accessible    = true
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  skip_final_snapshot    = true
+
+  tags = {
+    Name       = "PB - NOV 2024"
+    CostCenter = "C092000024"
+    Project    = "PB - NOV 2024"
+  }
+
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds_subnet_group"
+  subnet_ids = [aws_subnet.privada-app-A.id, aws_subnet.privada-app-B.id]
+  tags = {
+    Name = "rds_subnet_group"
+  }
+}
