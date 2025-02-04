082a54aa2e776f1421aacbb55383bf3aa39bdb10
Julio Moura <juliomourajr@hotmail.com>
2025-02-04 04:24:18 -0300

    :sparkles: feat: create alb.tf

---
+++ alb.tf
@@ -0,0 +1,54 @@
+resource "aws_lb" "wordpress_lb" {
+  name               = "wordpress-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.publica-elb-A.id, aws_subnet.publica-elb-B.id]
+
+  tags = {
+    Name = "wordpress-lb"
+  }
+}
+
+resource "aws_lb_listener" "wordpress_listener" {
+  load_balancer_arn = aws_lb.wordpress_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wordpress_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "wordpress_target_group" {
+  name     = "wordpress-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.compass_vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "wordpress-tg"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "wordpress_target_group_attachment_1a" {
+  target_group_arn = aws_lb_target_group.wordpress_target_group.arn
+  target_id        = aws_instance.wordpress-instance1a.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "wordpress_target_group_attachment_1b" {
+  target_group_arn = aws_lb_target_group.wordpress_target_group.arn
+  target_id        = aws_instance.wordpress-instance1b.id
+  port             = 80
+}
