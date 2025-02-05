a0d990e2662099a4302778466377f3aa997d61da
Julio Moura <juliomourajr@hotmail.com>
2025-02-05 03:40:36 -0300

    :recycle: refactor: Update auto scaling group.

--- asg.tf
+++ asg.tf
@@ -42,9 +42,9 @@ resource "aws_launch_template" "wordpress_template" {
 resource "aws_autoscaling_group" "wordpress_asg" {
   name                = "wordpress-asg"
   vpc_zone_identifier = [aws_subnet.privada-app-A.id, aws_subnet.privada-app-B.id]
-  desired_capacity    = 1
-  min_size            = 1
-  max_size            = 2
+  desired_capacity    = 2
+  min_size            = 2
+  max_size            = 4
   health_check_type   = "ELB"
   target_group_arns   = [aws_lb_target_group.wordpress_target_group.arn]
 
